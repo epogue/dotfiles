1830109e040d94b0acef1e763460da0106f1f2ff
Elliott Pogue <epogue@gmail.com>
2015-04-01 14:38:38 -0400

    Remove the need for Karabiner

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -20,6 +20,7 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 chflags nohidden ~/Library
 
 # Set a really fast key repeat.
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 defaults write NSGlobalDomain KeyRepeat -int 0
 
 # Set the Finder prefs for showing a few different volumes on the Desktop.
