c1ee6cfc319da7df89c11c635322406adb2a9afd
Elliott Pogue <epogue@gmail.com>
2017-06-18 12:58:51 -0400

    Update nvm installation

--- nvm/install.sh
+++ nvm/install.sh
@@ -1,15 +1,1 @@
-#!/bin/sh
-
-if test ! $(which nvm)
-then
-  echo "  Installing nvm for you."
-  brew install nvm > /tmp/nvm-install.log
-
-  if [ ! -d $HOME/.nvm ]
-  then
-    echo "  Copying nvm-exec"
-    mkdir $HOME/.nvm
-    cp $(brew --prefix nvm)/nvm-exec $HOME/.nvm/
-  fi
-fi
-
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
