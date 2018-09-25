ea0fb730ce7bdaf8b4a7f5c2a8f9f80c85c5fb75
Elliott Pogue <epogue@gmail.com>
2018-09-25 12:47:03 -0400

    Add pure and enable subpixel antialiasing on Mojave

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -7,6 +7,9 @@
 #
 # Run ./set-defaults.sh and you'll be good to go.
 
+# Re-enable subpixel anti-aliasing on Mojave
+defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
+
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
